9260165518897198f587a17f94fec2040c3b2f0a
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-04-27 16:34:56 -0700

    added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div><h4 className="footer__para">Project done by <Link mailto="manishareddykodicherla@gmail.com">Manisha</Link></h4>
+    <div><h4 className="footer__para">Project done by <a href="mailto:manishareddykodicherla@gmail.com">Manisha</a></h4>
     </div>
   )
 }
