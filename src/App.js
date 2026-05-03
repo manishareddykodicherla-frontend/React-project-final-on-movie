ff450eb74f968f53a2f3ff268f23f1318a66c64f
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-05-03 15:26:44 -0700

    Fix buid issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import './App.css';
+import "./App.css";
 import Nav from "./Components/Nav";
 import List from "./Components/List";
 import Filter from "./Components/Filter";
 import Input from "./Components/Input";
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import Footer from './Components/Footer';
-import Review from './Components/Review'
-import { Route } from 'react-router-dom';
-  
+import Footer from "./Components/Footer";
+import Review from "./Components/Review";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  
-  const searchMovies = useCallback(async (term = searchTerm) => {
-    setLoading(true);
-    const res = await axios.get(`https://www.omdbapi.com/?apikey=3608d43&s=${term}`);
-    setMovies(res.data.Search || []);
-    setLoading(false);
-  }, [searchTerm]);
+  const searchMovies = useCallback(
+    async (term = searchTerm) => {
+      setLoading(true);
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=3608d43&s=${term}`,
+      );
+      setMovies(res.data.Search || []);
+      setLoading(false);
+    },
+    [searchTerm],
+  );
 
   useEffect(() => {
     searchMovies();
@@ -29,7 +33,7 @@ function App() {
   function filterMovies(sortValue) {
     if (sortValue === "movie_title") {
       const sortedMovies = [...movies].sort((a, b) =>
-        a.Title.localeCompare(b.Title)
+        a.Title.localeCompare(b.Title),
       );
       setMovies(sortedMovies);
     }
@@ -37,31 +41,39 @@ function App() {
     if (sortValue === "MOVIE__YEAR__NEW__TO_OLD") {
       const sortedMovies = [...movies].sort(
         (a, b) =>
-          b.Year.toString(``).slice(0, 4) - a.Year.toString(``).slice(0, 4)
+          b.Year.toString(``).slice(0, 4) - a.Year.toString(``).slice(0, 4),
       );
       setMovies(sortedMovies);
     }
     if (sortValue === "MOVIE__YEAR__OLD__TO_NEW") {
       const sortedMovies = [...movies].sort(
         (a, b) =>
-          a.Year.toString(``).slice(0, 4) - b.Year.toString(``).slice(0, 4)
+          a.Year.toString(``).slice(0, 4) - b.Year.toString(``).slice(0, 4),
       );
       setMovies(sortedMovies);
     }
   }
   return (
-    <Router>
     <div className="App">
-      <Nav/>
-      <Switch>
-      <Route path="/movies" component={Review} />
-      <Input setSearchTerm={setSearchTerm} searchMovies={searchMovies} />
-      <Filter filterMovies={filterMovies} movies={movies} />
-      <List movies={movies} loading={loading} />
-      <Footer/>
-      </Switch>
+      <Nav />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Input
+                setSearchTerm={setSearchTerm}
+                searchMovies={searchMovies}
+              />
+              <Filter filterMovies={filterMovies} movies={movies} />
+              <List movies={movies} loading={loading} />
+            </>
+             }
+        />
+        <Route path="/movies" element={<Review />} />
+      </Routes>
+      <Footer />
     </div>
-    </Router>
   );
 }
 export default App;
---
+++ src/Components/Review.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Review() {
+  return (
+    <div>
+        <div className="review">
+            <p>this is page is created just for testing purpose and will be upadted later.</p>
+        </div>
+    </div>
+  )
+}
