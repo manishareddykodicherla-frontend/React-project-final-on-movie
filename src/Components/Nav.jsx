6dcaa53b90923ea0e1489683e43c33f09031fba5
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-05-03 16:06:30 -0700

    Fix buid issue

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import image from '../assets/1000_F_500611919_5wuf1qGRCubiXXxIa7og1fLLCyHi6qP9.jpg'
-
+import { useState } from "react";
  import {  Link } from 'react-router-dom';
 export default function Nav() {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
+
+    <div className="nav__container">
     <nav>
         <figure className="movie">
             <img src={image} alt=""className="navlogo"/>
         </figure>
-        <ul className="nav__lists">
+        <button className="burger" onClick={() => setOpen(!open)}>☰</button>
+        <ul className={open ? "nav__lists nav__lists--open" : "nav__lists"}>
             <li className="nav__list"><Link to="/">Home</Link></li>
            <li className="nav__list"><Link to="/movies">Movies</Link></li>
            <li className="nav__list"> <Link to="/tickets">Tickets</Link></li>
