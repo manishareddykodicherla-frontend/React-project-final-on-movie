ff935dfc9042957865c85fe39632fa60c9d0d760
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-05-03 14:48:57 -0700

    Fix buid issue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Input from "./Components/Input";
 import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import Footer from './Components/Footer';
-
+import Review from './Components/Review'
+import { Route } from 'react-router-dom';
   
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -36,26 +37,31 @@ function App() {
     if (sortValue === "MOVIE__YEAR__NEW__TO_OLD") {
       const sortedMovies = [...movies].sort(
         (a, b) =>
-          a.Year.toString(``).slice(0, 4) - b.Year.toString(``).slice(0, 4)
+          b.Year.toString(``).slice(0, 4) - a.Year.toString(``).slice(0, 4)
       );
       setMovies(sortedMovies);
     }
     if (sortValue === "MOVIE__YEAR__OLD__TO_NEW") {
       const sortedMovies = [...movies].sort(
         (a, b) =>
-          b.Year.toString(``).slice(0, 4) - a.Year.toString(``).slice(0, 4)
+          a.Year.toString(``).slice(0, 4) - b.Year.toString(``).slice(0, 4)
       );
       setMovies(sortedMovies);
     }
   }
   return (
+    <Router>
     <div className="App">
-      <Nav />
+      <Nav/>
+      <Switch>
+      <Route path="/movies" component={Review} />
       <Input setSearchTerm={setSearchTerm} searchMovies={searchMovies} />
       <Filter filterMovies={filterMovies} movies={movies} />
       <List movies={movies} loading={loading} />
       <Footer/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 export default App;
